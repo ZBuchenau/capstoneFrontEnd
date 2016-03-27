ae1df3e3be36e2e3b42f62ab37581b6866e7cba0
Zach Buchenau <zbuchenau@yahoo.com>
2016-03-26 22:07:44 -0600

    cleaned up a little bit of code for the graphs, and made it so the graphs reset every time I reload new data.

--- js/profileController.js
+++ js/profileController.js
@@ -6,6 +6,7 @@ function profileController($scope, $http, $route, $routeParams, $location, $wind
   vm.show = false;
 
   vm.series = [];
+  vm.site = [];
 
   //========================================================================================
   // This is all the user data that will eventually be used in the graphs
@@ -23,55 +24,77 @@ function profileController($scope, $http, $route, $routeParams, $location, $wind
   vm.bounceRate = [[]];
 
 
-//========================================================================================
-// Return all accounts our app has access to for that user.
-//========================================================================================
+  //========================================================================================
+  // Return all accounts our app has access to for that user.
+  //========================================================================================
   vm.accounts = accountData.getAccountData(vm.dataObject).then(onSuccess, onFailure);
 
-    function onSuccess(response) {
-      console.log(response);
-      vm.series = [response.data[0].account_name];
-      console.log(vm.series);
-      vm.allAccounts = response.data;
-      return vm.allAccounts;
-    }
+  function onSuccess(response) {
+    console.log(response);
+    vm.series = [response.data[0].account_name];
+    console.log(vm.series);
+    vm.allAccounts = response.data;
+    return vm.allAccounts;
+  }
 
-    function onFailure(response) {
-      console.log('There was an error in your request...');
-    }
+  function onFailure(response) {
+    console.log('There was an error in your request...');
+  }
 
 
-//=========================================================================================
-// Making a request to the server to return 30 days worth of data for the selected account.
-// On success, loop through the response, and push all items into their respective arrays.
-//=========================================================================================
-  vm.coreData = function(param){
+  //=========================================================================================
+  // Making a request to the server to return 30 days worth of data for the selected account.
+  // On success, loop through the response, and push all items into their respective arrays.
+  //=========================================================================================
+  vm.coreData = function(param) {
+
+    chartData = [];
+    vm.dates = [];
+    vm.sessions = [[]];
+    vm.users = [[]];
+    vm.sessionsPerUser = [[]];
+    vm.newSessions = [[]];
+    vm.pageviews = [[]];
+    vm.pagesPerSession = [[]];
+    vm.atos = [[]];
+    vm.bounceRate = [[]];
+
     // console.log(param);
     accountData.getCoreData(param).then(success, failure);
 
-      function success(response) {
-        chartData.push(response.data);
-        vm.seriesOne = vm.site.account_name;
-        console.log('This is the chart data: ', chartData[0]);
-        vm.show = true;
-        var graphData = chartData[0];
+    function success(response) {
+      chartData.push(response.data);
+      vm.seriesOne = vm.site.account_name;
+      console.log('This is the chart data: ', chartData[0]);
+      vm.show = true;
+      var graphData = chartData[0];
 
-        for(var i = 0; i < graphData.length; i++){
-            vm.dates.push(graphData[i].date);
-            vm.sessions[0].push(graphData[i].sessions);
-            vm.users[0].push(graphData[i].users);
-            vm.sessionsPerUser[0].push(graphData[i].sessions_per_user);
-            vm.pageviews[0].push(graphData[i].pageviews);
-            vm.pagesPerSession[0].push(graphData[i].pages_per_session);
-            vm.atos[0].push(graphData[i].atos);
-            vm.bounceRate[0].push(graphData[i].bounce_rate);
-            vm.newSessions[0].push(graphData[i].new_sessions);
-          }
-        }
 
-      function failure(response) {
-        console.log('ERROR');
+
+
+      for (var i = 0; i < graphData.length; i++) {
+        var dateNumbers = graphData[i].date;
+        var date = dateNumbers.toString();
+        var year = date.substring(0, 4);
+        var month = date.substring(4, 6);
+        var day = date.substring(6, 8);
+        var actualDate = month + "/" + day + "/" + year;
+
+        vm.dates.push(actualDate);
+        vm.sessions[0].push(graphData[i].sessions);
+        vm.users[0].push(graphData[i].users);
+        vm.sessionsPerUser[0].push(graphData[i].sessions_per_user);
+        vm.pageviews[0].push(graphData[i].pageviews);
+        vm.pagesPerSession[0].push(graphData[i].pages_per_session);
+        vm.atos[0].push(graphData[i].atos);
+        vm.bounceRate[0].push(graphData[i].bounce_rate);
+        vm.newSessions[0].push(graphData[i].new_sessions);
       }
+    }
+
+    function failure(response) {
+      console.log('ERROR');
+    }
   };
 
 
